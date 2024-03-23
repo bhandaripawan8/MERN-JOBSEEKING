02612e400f4f30ef348001aede553736c91ec203
Pawan <pawanb78@gmail.com>
2024-03-23 12:09:57 +0000

    mongoose model for jobschema created

---
+++ backend/model/Job.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const jobSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: [true, "Please provide a job title"],
+        minLength: [3, "Job title must be at least three characters"],
+        maxLength: [50, "Job title must not exceed 50 characters"],
+    },
+    description: {
+        type: String,
+        required: [true, "Please provide the job description"],
+        minLength: [3, "Job description must be at least 3 characters"],
+        maxLength: [300, "Job description must not exceed 300 characters"]
+    },
+    category: {
+        type: String,
+        required: [true, "job Category is required"]
+    },
+    country: {
+        type: String,
+        required: [true, "Job country is required"]
+    },
+    city: {
+        type: String,
+        required: [true, "Please provide the city name"]
+    },
+    location: {
+        type: String,
+        required: [true, "Please provide exact location"],
+        minLength: [50, "Job location must contain at least 50 characters"]
+    },
+    fixedSalary: {
+        type: Number,
+        minLength: [4, "Fixed salary must contain at least 4 digits"],
+        maxLength: [4, "Fixed salary must not exceed at least 9 digits"],
+    }
+})
--- backend/routes/UserRouter.js
+++ backend/routes/UserRouter.js
@@ -2,11 +2,12 @@
 
 import express from 'express';
 import {register, login, logout} from '../controllers/UserController.js'
+import {isAuthorised} from '../middlewares/Auth.js'
 
 const router = express.Router();
 router.post('/register', register);
 router.post('/login', login);
-router.get('/logout', logout);
+router.get('/logout', isAuthorised,logout);
 
 
 export default router;
