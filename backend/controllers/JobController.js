d9c06b59357572b4b85d0f3392e0118470e32638
Pawan <pawanb78@gmail.com>
2024-03-24 11:54:36 +0000

    controller for update job done

--- backend/controllers/JobController.js
+++ backend/controllers/JobController.js
@@ -91,7 +91,22 @@ export const postJob = async (req, res, next) => {
   export const updateJob = catchAsyncError(async(req, res, next) =>{
     const {role} = req.user;
     if(role === "job seeker"){
-        return next(new ErrorHandler(""))
+        return next(new ErrorHandler("job seeker is not allowed to update the job", 400))
     }
+    const {id} = req.params;
+    let job = await Job.findById(id);
+    if(!job){
+        return next(new ErrorHandler('Oops, no jobs found', 404))
+    }
+    job = await Job.findByIdAndUpdate(id, req.body, {
+        new: true, 
+        runValidators: true,
+        useFindAndModify: false,
+    })
+    res.status(200).json({
+        success: true,
+        job,
+        message: 'Job updated successfully!'
+    })
   })
   
