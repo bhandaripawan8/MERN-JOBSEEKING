61fbf1ca01df5cf04e6be05ad5dfb7bdd8d039fb
Pawan <pawanb78@gmail.com>
2024-03-24 13:10:27 +0000

    application controller and routes done

--- backend/controllers/ApplicationController.js
+++ backend/controllers/ApplicationController.js
@@ -1,6 +1,6 @@
-import { catchAsyncError } from "../middlewares/catchAsyncError";
-import ErrorHandler from "../middlewares/error";
-import { Application } from "../model/ApplicationSchema";
+import { catchAsyncError } from "../middlewares/catchAsyncError.js";
+import ErrorHandler from "../middlewares/error.js";
+import { Application } from "../model/ApplicationSchema.js";
 
 export const emploerGetAllApplications = catchAsyncError(async(req, res,next) =>{
     const {role} = req.user;
@@ -13,4 +13,32 @@ export const emploerGetAllApplications = catchAsyncError(async(req, res,next) =>
         success: true,
         applications
     })
+})
+
+export const jobSeekerGetAllApplications = catchAsyncError(async(req, res,next) =>{
+    const {role} = req.user;
+    if(role === "employer"){
+        return (next (new ErrorHandler("employer is not allowed to access to this resources", 400)))
+    }
+    const {_id} = req.user;
+    const applications = await Application.find({'applicantId.user': _id})
+    res.status(200).json({
+        success: true,
+        applications
+    })
+})
+
+export const jobSeekerDeleteApplication = catchAsyncError(async(req, res, next)=>{
+    const {role} = user.role;
+    if(role === 'employer'){
+        return next(new ErrorHandler("Employer is not allowed to delete the applications", 404))
+    }
+    const {id} = req.params;
+    const application = await Application.findById(id);
+    if(!application){
+        return next(new ErrorHandler('Oops, application not found!', 404))
+    }
+    await application.deleteOne();
+    res.status(200).json({status: true, message: 'Application deleted successfully'})
+
 })
--- backend/routes/ApplicationsRouter.js
+++ backend/routes/ApplicationsRouter.js
@@ -1,6 +1,12 @@
 
 import express from 'express';
+import {emploerGetAllApplications, jobSeekerDeleteApplication, jobSeekerGetAllApplications} from '../controllers/ApplicationController.js'
+import { isAuthorised } from '../middlewares/Auth.js';
 
 const router = express.Router();
+
+router.get('/job/jobseeker/getall', jobSeekerDeleteApplication)
+router.get('/job/employer/getall', emploerGetAllApplications)
+router.delete('/job/delete/:id', isAuthorised,jobSeekerDeleteApplication)
 
 export default router;
