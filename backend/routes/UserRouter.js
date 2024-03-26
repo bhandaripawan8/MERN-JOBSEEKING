77698112f70640a72b23a5993c95588ca4d5927d
Pawan <pawanb78@gmail.com>
2024-03-26 23:54:40 +0000

    react installed, folder structure done

--- backend/routes/UserRouter.js
+++ backend/routes/UserRouter.js
@@ -1,14 +1,14 @@
 
 
 import express from 'express';
-import {register, login, logout, getUser} from '../controllers/UserController.js'
+import {register, login, logout, User} from '../controllers/UserController.js'
 import {isAuthorised} from '../middlewares/Auth.js'
 
 const router = express.Router();
 router.post('/register', register);
 router.post('/login', login);
 router.get('/logout', isAuthorised, logout);
-router.get('/getUser', isAuthorised, getUser);
+router.get('/getuser', isAuthorised, User);
 
 
 export default router;
